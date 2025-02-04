1e686d54d5905bc0f3d40dca9aec21357b1b317b
Merchant151 <joshntram@gmail.com>
2025-02-03 20:25:00 -0500

    basic content script tinkering

--- content-script.js
+++ content-script.js
@@ -1,4 +1,11 @@
-chrome.dom.openOrClosedShadowRoot(
-	  element: HTMLElement,
-)
-
+//chrome.dom.openOrClosedShadowRoot(
+//	  element: HTMLElement,
+//)
+async function testlog(){
+	while(true){
+		console.log('heartbeat');
+		await new Promise(resolve => setTimeout(resolve, 2000));
+	}
+}
+console.log('Content-scripts.js starts here! Hello, world');
+testlog();
