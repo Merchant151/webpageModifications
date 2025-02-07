0520ec125885168c90c1fa28701e6bb84dd2e29a
Merchant151 <joshntram@gmail.com>
2025-02-06 20:34:44 -0500

    experimented with manipulating the dom from content script

--- content-script.js
+++ content-script.js
@@ -4,25 +4,42 @@
 async function testlog(){
 	while(true){
 		console.log('heartbeat');
-		await new Promise(resolve => setTimeout(resolve, 2000));
+		await new Promise(resolve => setTimeout(resolve, 12000));
+		//move single elem arround 
+		document.body.getElementsByTagName("*")[8].appendChild(insertElement);
 	}
 }
 console.log('Content-scripts.js starts here! ELEM TEST');
 testlog();
 // Create a button element
-//const button = document.createElement("button");
+const button = document.createElement("button");
 
 // Set the button's text content
-//button.textContent = "Click me";
+button.textContent = "Click me";
 
 // Set the button's ID
-//button.id = "clickMe";
+button.id = "clickMe";
 
 // Append the button to the document body
-//document.body.appendChild(button);
+document.body.appendChild(button);
 
 const getElemTest = document.body;
 
-console.log('testing get elm body');
-console.log('testing elm?: '+JSON.parse(getElemTest));
+console.log('testing get elm');
+//console.log('testing elm?: '+JSON.parse(getElemTest));
+//console.log('testing elm content'+JSON.parse(getElemTest.innerHTML));
+//this seems to be returning the actual html object which can be intereacted with. 
+console.log(document.body);
+console.log(getElemTest);
+//looks like getting the all element belonging to the body as text which can be easily parced
+console.log(getElemTest.innerHTML);
+/// Get all elems 
+//console.log(document.body.getElementsByTagName("*"));
+console.log(document.body.getElementsByTagName("*")[8]);
+console.log('elm testing complete');
 
+
+const insertElement = document.createElement('p');
+insertElement.textContent = "This text was inserted to the page"
+document.body.appendChild(insertElement);
+//document.body.getElementsByTagName("*")[8].appendChild(insertElement);
