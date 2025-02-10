9dc0c5019309631d121f491239bc59066cf9c16f
Merchant151 <joshntram@gmail.com>
2025-02-09 20:01:00 -0500

    content script that can grab google results

---
+++ getResults.js
@@ -0,0 +1,9 @@
+//Not the best way to search Query selecter is prob better
+//const elmBucket = document.body.getElementsByTagName('span');i
+//example for google <span jscontroller="msmzHf" jsaction="rcuQ6b:npT2md;PYDNKe:bLV6Bd;mLt3mc">
+const elmBucket = document.querySelectorAll('[jsaction="rcuQ6b:npT2md;PYDNKe:bLV6Bd;mLt3mc"]');
+
+
+console.log("Find getresults.js");
+console.log(elmBucket);
+console.log("end getresults.js");
