1b6b44822b118274835ebd7dff8c4e0969c28563
Merchant151 <joshntram@gmail.com>
2025-02-12 09:25:11 -0500

    readStorage.js working on getting data

--- readStorage.js
+++ readStorage.js
@@ -1,3 +1,19 @@
 const clear = document.getElementByID('clear');
 
-clear.addEventListener('onClick', function() {console.log('do something')});
+clear.addEventListener('onClick', function() {
+
+	console.log('do something');
+
+	});
+
+
+async function read(){
+	//let data = await chrome.storage.local.get(null);
+	//data_keys = Object.keys(data);
+	let data = [1,2,3]
+	for (var store = 0;store < data.length;store)
+
+
+};
+
+async function delete(){};
