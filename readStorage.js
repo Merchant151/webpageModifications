afa09ca7f3a72c7ea1b4e1223d4930bb9e020193
Merchant151 <joshntram@gmail.com>
2025-02-13 09:46:48 -0500

    changing storage changing

--- readStorage.js
+++ readStorage.js
@@ -8,10 +8,15 @@ clear.addEventListener('onClick', function() {
 
 
 async function read(){
-	//let data = await chrome.storage.local.get(null);
-	//data_keys = Object.keys(data);
-	let data = [1,2,3]
-	for (var store = 0;store < data.length;store)
+	let data = await chrome.storage.local.get(null);
+	data_keys = Object.keys(data);
+	console.log(data_keys);
+	let data = [1,2,3];
+	for (var store = 0;store < data.length;store++){
+		console.log('doing nothing')
+		
+
+	}
 
 
 };
