28c6563592b5edaa5c6b4a37b17b3658a2dfc8c7
Merchant151 <joshntram@gmail.com>
2025-02-25 13:23:15 -0500

    working on getting the link of result

--- getResults.js
+++ getResults.js
@@ -2,20 +2,23 @@
 //const elmBucket = document.body.getElementsByTagName('span');i
 //example for google <span jscontroller="msmzHf" jsaction="rcuQ6b:npT2md;PYDNKe:bLV6Bd;mLt3mc">
 //I have identified result text in a h3 elm with class "LC20lb MBeuO DKV0Md"
+//I have identified attribute jsname='UWckNb' 
 async function main(){
 	await new Promise(resolve => setTimeout(resolve, 3000));
 	console.log('ran timeout  so get results starts with a delay')
 	const elmBucket = document.querySelectorAll('[jsaction="rcuQ6b:npT2md;PYDNKe:bLV6Bd;mLt3mc"]');
-
+	//elmBucket is a set of google results 'typically ~10 per search'
 
 	console.log("Find getresults.js");
 	console.log(elmBucket);/// This returns a node list 
 
 	console.log(elmBucket[0]);
-	console.log('Logging innerChildren');
-	console.log(elmBucket[0].innerHTML);
+	console.log(elmBucket[].getHTML());
+	//console.log('Logging innerChildren');
+	//console.log(elmBucket[0].innerHTML);
 	const resultClassElm = elmBucket[0].getElementsByClassName('LC20lb MBeuO DKV0Md');
-	console.log(resultClassElm[0].textContent);
+	//This elm class contains the reuslt text 
+	//console.log(resultClassElm[0].textContent);
 	const resultText = resultClassElm[0].textContent;
 	console.log("end getresults.js");
 
