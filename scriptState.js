7fa9af67479d19f77f715ba5e86d155b6935aee5
Merchant151 <joshntram@gmail.com>
2025-03-24 08:45:22 -0400

    making progress but initial trigger seems to have stopped working

--- scriptState.js
+++ scriptState.js
@@ -9,18 +9,20 @@
 //TODO: I would like to change this to be done by looping 
 //over a list of elements instead
 const scriptToggles = document.getElementById('toggle');
+let startup = true;
 
 //scriptToggles.click();
 async function updateScriptState(){
 	//when script state is null we need to set the script to active or inactive
-	let scriptState = await chrome.storage.local.get(["script1"]);
-	console.log("Script state: "+ scriptState.key);
-	if ("undefined"==""+scriptState.key){
+	let scriptState = await chrome.storage.local.get("script1");
+	console.log("Script state: "+ scriptState.script1);
+	if ("undefined"==""+scriptState.script1){
 		chrome.storage.local.set({script1: "active"});	
-		scriptToggles.click();
+		await scriptToggles.click();
 	}else if ("active" == scriptState.key){
-		scriptToggles.click();
+		await scriptToggles.click();
 	}
+	startup = false;
 };
 updateScriptState();
 //main sets tickbox from storage 
@@ -30,14 +32,18 @@ updateScriptState();
 //listner sets script based on button id 
 scriptToggles.addEventListener('click', async function() {
 	console.log('test setting state to storage')
-	let scriptState = await chrome.storage.local.get([script1]);
-	console.log(""+scriptState.key)
-	if ("active" == scriptState.key){
+	let scriptState = await chrome.storage.local.get("script1");
+	console.log(""+scriptState.script1)
+	if (!startup){
+	if ("active" == scriptState.script1){
 		console.log("inactive triggered")
 		chrome.storage.local.set({script1: "inactive"});
 	}else{
 		console.log("active triggered")
 		chrome.storage.local.set({script1:"active"});
+	}
+	}else{
+	console.log('startup true')
 	}
 	//update crome store based on selection/
 	//chrome.storage.local.set();
